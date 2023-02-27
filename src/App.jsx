ca133fd900700c6c02e92ac90f7a8b54d91aab6d
MoniIgnacio <moniignacio02@gmail.com>
2023-02-27 16:20:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="page">
+      <header>
+        <h1>Movies</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
