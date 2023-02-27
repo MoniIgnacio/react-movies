42864a564d900c4c715380eb02b43b17a943d061
MoniIgnacio <moniignacio02@gmail.com>
2023-02-27 23:22:11 +0100

    add customHook & service with fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,95 @@
-import "./App.css";
-import results from "./mocks/results.json";
-import no_results from "./mocks/no_results.json";
+import './App.css'
+import { useMovies } from './hooks/useMovies.js'
+import { Movies } from './components/Movies.jsx'
+import { useState, useEffect, useRef, useCallback } from 'react'
+import debounce from 'just-debounce-it'
 
-function App() {
-  const movies = results.Search;
-  const hasMovies = movies?.length > 0;
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+
+function App () {
+  const [sort, setSort] = useState(false)
+
+  const { search, updateSearch, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ search, sort })
+
+  // const debouncedGetMovies = useCallback(
+  //   debounce(search => {
+  //     console.log('search', search)
+  //     getMovies({ search })
+  //   }, 300)
+  //   , [getMovies]
+  // )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  // const handleSort = () => {
+  //   setSort(!sort)
+  // }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debouncedGetMovies(newSearch)
+  }
+
   return (
-    <div className="page">
-      <h1>Movies</h1>
+    <div className='page'>
+
       <header>
-        <form className="form">
-          <input placeholder="Avenger, Star Wars" />
-          <button type="submit">Buscar</button>
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...'
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
+
       <main>
-        {hasMovies ? (
-          <ul>
-            {movies.map((movie) => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No se encontraron peliculas para esta busqueda</p>
-        )}
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
       </main>
     </div>
-  );
+  )
 }
-// 5ebcfa3c
-// http://www.omdbapi.com/?apikey=[5ebcfa3c]&s=
 
-export default App;
+export default App
